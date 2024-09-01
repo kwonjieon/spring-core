d2a51cb73747e37f2237aaa971963184821839e5
kwonjieon <holemart@naver.com>
2024-09-01 21:56:49 +0900

    feat:AppConfig 리팩터링

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,5 +1,6 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
@@ -11,10 +12,18 @@ import hello.core.order.OrderServiceImpl;
 public class AppConfig {//실제 동작에 필요한 구현객체 생성 클래스
 
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private static MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(),new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private static DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
