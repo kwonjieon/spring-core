d97cf53c7d5cf36f2a51a9a00fa8e0ad45b6329a
kwonjieon <holemart@naver.com>
2024-09-01 21:25:20 +0900

    feat:AppConfig를 통하여 관심사 분리

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -1,15 +1,13 @@
 package hello.core;
 
-import hello.core.member.Grade;
-import hello.core.member.Member;
-import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
+import hello.core.member.*;
 
 public class MemberApp {
     public static void main(String[] args){
-        MemberService memberService = new MemberServiceImpl();
-        Member member = new Member(1L, "memberA", Grade.VIP);
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService= appConfig.memberService();
 
+        Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
         Member findMember = memberService.findMember(1L);
