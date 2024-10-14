ff8b82c9aa55452ee365ceee1eb85f0b62a83cf7
kwonjieon <holemart@naver.com>
2024-10-14 22:35:42 +0900

    @Bean(initMethod = "init",destroyMethod = "close")를 사용한 초기화,소멸

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,9 +1,7 @@
 package hello.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
     private String url;
 
     public NetworkClient() {
@@ -25,16 +23,16 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close "+url);
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception {//의존관계주입이 끝나면 실행
-        System.out.println("NetworkClient.afterPropertiesSet");
+
+    public void init(){//의존관계주입이 끝나면 실행
+        System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
-    @Override
-    public void destroy() throws Exception {//빈이 종료될때
-        System.out.println("NetworkClient.destroy");
+
+    public void close(){//빈이 종료될때
+        System.out.println("NetworkClient.close");
         disconnect();
     }
 }
